a0436adf9a035b690c59a1292ba3d256d715bf0d
Gian <gian.defaria@hotmail.com>
2022-11-03 23:55:54 -0300

    Adicionei três eventListener para realizar o cálculo ao levantar uma tecla e ao clicar. Tive que adicionar uma verificação especial para o campo de número de pessoas e, para isso, tive que deixar uma variável fora do For para gravar o número do índice

--- script.js
+++ script.js
@@ -9,16 +9,54 @@ bill.addEventListener('keyup', () => {
     tipAmountOutput.value = ('$' + bill.value);
 })
 
+var test;
 for (let i = 0; i < tipValue.length; i++) {
-    const value = tipValue[i].value;
 
-    tipValue[i].addEventListener('click', () => {
-        tipAmountOutput.value = ('$' + (bill.value * tipValue[i].value / numberPeople.value).toFixed(2));
+
+  tipValue[i].addEventListener('click', () => {
+        tipAmountOutput.value = ('$' + (bill.value * (tipValue[i].value / 100) / numberPeople.value).toFixed(2));
         tipTotalOutput.value = ('$' + ((bill.value / numberPeople.value) + Number(tipAmountOutput.value.slice(1,6))).toFixed(2));
+        test = i;
     })
+    tipValue[i].addEventListener('keyup', () => {
+        tipAmountOutput.value = ('$' + (bill.value * (tipValue[i].value / 100) / numberPeople.value).toFixed(2));
+        tipTotalOutput.value = ('$' + ((bill.value / numberPeople.value) + Number(tipAmountOutput.value.slice(1,6))).toFixed(2));
+        test = i;
+        
+    })
+    numberPeople.addEventListener('keyup', () => {
+        tipAmountOutput.value = ('$' + (bill.value * (tipValue[test].value / 100) / numberPeople.value).toFixed(2));
+        tipTotalOutput.value = ('$' + ((bill.value / numberPeople.value) + Number(tipAmountOutput.value.slice(1,6))).toFixed(2));
+        
+        
+    })
+   
 }
 
+const errorMessage = document.getElementById('error1');
 
-function outputValue() {
-    tipAmountOutput.value = inputValue.value;
+function verifyNumberPeople () {
+
+    if (numberPeople.value == 0 || numberPeople.value == "") {
+
+        errorMessage.innerText = "teste";
+            
+    } else {
+        errorMessage.innerText = "";
+    }
+}
+ 
+    
+function reset () {
+
+    let resetNumber = document.querySelectorAll('.reset');
+    let i = 0;
+    while (i < resetNumber.length) {
+
+        resetNumber[i].value = "";
+        i++;        
+    }
+    tipAmountOutput.value = "$0.00"
+    tipTotalOutput.value = "$0.00";
+
 }
